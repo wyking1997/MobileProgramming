b10da35957ac106620f65da3558e2da75575af86
Unknown <marius.comiati@yahoo.com>
2017-11-10 02:09:48 +0200

    Changes to addMovie layout

---
+++ CinemaCluj/app/src/main/java/com/cluj/cinema/marius/cinemacluj/CreateNewItemActivity.java
@@ -0,0 +1,13 @@
+package com.cluj.cinema.marius.cinemacluj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNewItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_item);
+    }
+}
