19e076c7c39854658a36db51ec57b61bdd6f9391
Unknown <marius.comiati@yahoo.com>
2017-11-10 22:32:42 +0200

    ReactNative all movies shown

--- CinemaReact/App.js
+++ CinemaReact/App.js
@@ -22,109 +22,105 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+// react-native run-android
+// Alert.alert('You tapped ' + item.title)
+
 export default class App extends Component<{}> {
-    render() {
-        return (
-            <FlatList style={{flex: 1, padding: 8}}
-                      data={[
-                          {key: 'Devin'},
-                          {key: 'Jackson'},
-                          {key: 'James'},
-                          {key: 'Joel'},
-                          {key: 'John'},
-                          {key: 'Jillian'},
-                          {key: 'Jimmy'},
-                          {key: 'Julie'},
-                          {key: 'Devisn'},
-                          {key: 'Jacksson'},
-                          {key: 'Jamess'},
-                          {key: 'Joels'},
-                          {key: 'Johns'},
-                          {key: 'Jillsian'},
-                          {key: 'Jimmsy'},
-                          {key: 'Julise'},
-                          {key: 'Devdsin'},
-                          {key: 'Jacsdkson'},
-                          {key: 'Jasdmes'},
-                          {key: 'Josdel'},
-                          {key: 'Josdhn'},
-                          {key: 'Jisdllian'},
-                          {key: 'Jimsdmy'},
-                          {key: 'Julsdie'},
-                          {key: 'Devdqsin'},
-                          {key: 'Jacsqdkson'},
-                          {key: 'Jasqdmes'},
-                          {key: 'Josqdel'},
-                          {key: 'Josqdhn'},
-                          {key: 'Jisqdllian'},
-                          {key: 'Jimsqdmy'},
-                          {key: 'Julsqdie'},
-                          {key: 'Dasevin'},
-                          {key: 'Jacaskson'},
-                          {key: 'Jamesas'},
-                          {key: 'Joelas'},
-                          {key: 'asJohn'},
-                          {key: 'Jisallian'},
-                          {key: 'Jimmasy'},
-                          {key: 'Jsaulie'},
-                          {key: 'Defdvisn'},
-                          {key: 'Jacgfksson'},
-                          {key: 'Jamehgss'},
-                          {key: 'Joelshg'},
-                          {key: 'Johnshg'},
-                          {key: 'Jilhghglsian'},
-                          {key: 'Jimmshgy'},
-                          {key: 'Julisdfse'},
-                          {key: 'Devdsigfdn'},
-                          {key: 'Jacsdksfdson'},
-                          {key: 'Jasdmefsdfs'},
-                          {key: 'Josdefsdfl'},
-                          {key: 'Josdhfsdn'},
-                          {key: 'Jisdllifsan'},
-                          {key: 'Jimsdmfdsy'},
-                          {key: 'Julsdisdfe'},
-                          {key: 'Devdqsisdfn'},
-                          {key: 'Jacsqdkssdfon'},
-                          {key: 'Jasqdmesfds'},
-                          {key: 'Josqdesfdl'},
-                          {key: 'Josqdhfsdfdsn'},
-                          {key: 'Jisqdlliafdsn'},
-                          {key: 'Jimsqdmfsdy'},
-                          {key: 'Julsqdifsde'},
-                      ]} renderItem={({item}) => <View style={{height: 40, backgroundColor: 'powderblue'}}>
-                <Button
-                    onPress={() => { Alert.alert('You tapped ' + item.key)}}
-                    title={item.key}
-                />
-            </View>}
+
+    constructor(props){
+        super(props);
+
+        this.getMovies = this.getMovies.bind(this);
+        this.setDetailView = this.setDetailView.bind(this);
+        this.getMovieListElement = this.getMovieListElement.bind(this);
+        this.setDetailView = this.setDetailView.bind(this);
+        this.setCreateNewMovie = this.setCreateNewMovie.bind(this);
+
+        movies = this.getMovies();
+        element = this.getMovieListElement(movies);
+        this.state = {movies: movies, element: element};
+    }
+
+    setDetailView(movieId){
+        movie = this.state.movies.find(m => m.id === movieId);
+        Alert.alert("" + movie);
+    }
+
+    setMovieListView(){
+        this.setState(this.getMovieListElement);
+    }
+
+    setCreateNewMovie(){
+
+    }
+
+    getMovieListElement(movies){
+        myMovies = this.getMovieForFlatList(movies);
+        return (<View style={styles.mainView}>
+            <FlatList
+                style={styles.listView}
+                data={myMovies}
+                renderItem={({item}) =>
+                    <TouchableHighlight onPress={() => {}} underlayColor="azure">
+                        <View style={styles.listItemView}>
+                            <Text style={styles.bigBlack}>
+                                {item.title + " " + item.year + "\n" + item.duration + " minutes"}
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+                }
             />
+            <Button
+                syle={{padding: 10}}
+                title="Create New Movie"
+                onPress={() => {}}
+            />
+        </View>);
+    }
+
+    getMovies(){
+        return [
+            {id: 0, year: 2017, title: "Piratii din caraibe", duration: 95, description: "betiv norocos"},
+            {id: 1, year: 2017, title: "Omul paianjen", duration: 80, description: "This is with benner!"},
+            {id: 2, year: 2017, title: "Thor", duration: 115, description: "big green animal"},
+            {id: 3, year: 2008, title: "Neinfricata", duration: 65, description: "roscata si trage cu arcul"},
+            {id: 4, year: 2010, title: "Minionii 1", duration: 87, description: "galbeni si multi 1"},
+            {id: 5, year: 2011, title: "Minionii 2", duration: 71, description: "galbeni si multi 2"},
+            {id: 6, year: 2017, title: "Piratii din caraibe", duration: 95, description: "betiv norocos"},
+            {id: 7, year: 2017, title: "Omul paianjen", duration: 80, description: "This is with benner!"},
+            {id: 8, year: 2017, title: "Thor", duration: 115, description: "big green animal"},
+            {id: 9, year: 2008, title: "Neinfricata", duration: 65, description: "roscata si trage cu arcul"},
+            {id: 10, year: 2010, title: "Minionii 1", duration: 87, description: "galbeni si multi 1"},
+            {id: 11, year: 2011, title: "Minionii 2", duration: 71, description: "galbeni si multi 2"},
+        ];
+    }
+
+    getMovieForFlatList(list){
+        list.map(x => x.key = x.id);
+        return list;
+    }
+
+    render() {
+        return this.state.movies === null ? null : (
+            this.state.element
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+    mainView: {
+        flex: 1
     },
-    welcome: {
+    listView: {
+        padding: 10,
+        flex: 0.6,
+        backgroundColor: 'white',
+    },
+    listItemView: {
+        padding: 10
+    },
+    bigBlack: {
         fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    bigblue: {
-        color: 'blue',
         fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
     },
 });
